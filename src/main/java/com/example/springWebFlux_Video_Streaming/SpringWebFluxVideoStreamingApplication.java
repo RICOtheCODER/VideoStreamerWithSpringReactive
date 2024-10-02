8e68921fe7ddcabf755c48437a2b4066e53005ad
Saurajit Sarkar <djrico189@gmail.com>
2024-10-02 17:51:59 +0530

    first commit

---
+++ src/main/java/com/example/springWebFlux_Video_Streaming/SpringWebFluxVideoStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springWebFlux_Video_Streaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxVideoStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxVideoStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springWebFlux_Video_Streaming/SpringWebFluxVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springWebFlux_Video_Streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
